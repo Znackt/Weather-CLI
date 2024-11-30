63e9e402d6673b1db196cdd65d11b9b9ca340fa4
Znackt <Znackt@proton.me>
2024-12-01 02:05:33 +0530

    Step-4 Extract the weather response from the response

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::{fmt::format, io, string};
+use colored::*;
+use dotenv::dotenv;
 use reqwest::Response;
 use serde::Deserialize;
-use colored::*;
+use std::env;
+use std::{fmt::format, io};
 
-// Struct to deserialise the json response from openWeatherMap API 
+// Struct to deserialise the json response from openWeatherMap API
 #[derive(Deserialize, Debug)]
 struct WeatherData {
     weather: Vec<Weather>,
@@ -18,7 +20,7 @@ struct Weather {
     description: String,
 }
 
-// Struct to represent the main weather paramaters 
+// Struct to represent the main weather paramaters
 #[derive(Deserialize, Debug)]
 struct Main {
     temp: f64,
@@ -29,18 +31,33 @@ struct Main {
 //Struct to represent the wind information
 #[derive(Deserialize, Debug)]
 struct Wind {
-    speed: f64
+    speed: f64,
 }
 
 // function to get weather info from OpenWeatherMap API
-fn get_weather_info(city: &str, country_code: &str, api_key: &str) ->
- Result<WeatherData, reqwest::Error> {
-    let url: String = format!(
-        "http://api.openweathermap.org/data/2.5/forecast?q={},{}&units=metric&appid={}",
+fn get_weather_info(
+    city: &str,
+    country_code: &str,
+    api_key: &str,
+) -> Result<WeatherData, reqwest::Error> {
+    let url = format!(
+        "http://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid={}",
         city, country_code, api_key
     );
     let response = reqwest::blocking::get(&url)?;
-    let response_json: WeatherData = Response.json::<WeatherData>?;
-    Ok(response_json);
+    let response_json = response.json::<WeatherData>()?;
+    Ok(response_json)
+}
+
+// Function to display weather information
+fn display_weather_info(response: &WeatherData) {
+    // Extract the weather response from the response
+    let description: &String = &response.weather[0].description;
+    let tempreture: f64 = response.main.temp;
+    let humidity: f64 = response.main.humidity;
+    let pressure: f64 = response.main.pressure;
+    let wind_speed: f64 = response.wind.speed;
+
+    
 }
 
