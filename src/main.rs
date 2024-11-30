3c546836c7dbdc25c669808dfa0ad7aea128da0d
Znackt <Znackt@proton.me>
2024-12-01 00:20:04 +0530

    Step-2 => Created the structs to deseralise the json data from OpenWeatherMap API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use serde::Deserialize;
+use colored::*;
+
+// Struct to deserialise the json response from openWeatherMap API 
+#[derive(Deserialize, Debug)]
+struct WeatherData {
+    weather: Vec<Weather>,
+    main: Main,
+    wind: Wind,
+    name: String,
+}
+
+// Struct to represent weather description
+#[derive(Deserialize, Debug)]
+struct Weather {
+    description: String,
+}
+
+// Struct to represent the main weather paramaters 
+#[derive(Deserialize, Debug)]
+struct Main {
+    temp: f64,
+    humidity: f64,
+    pressure: f64,
+}
+
+//Struct to represent the wind information
+#[derive(Deserialize, Debug)]
+struct Wind {
+    speed: f64
 }
